b2270b0b4ff64ce134e6b33fcec16fe76674ce7a
martina <martipnn@gmail.com>
2025-12-15 11:19:17 +0100

    resolved for astar

--- robot_planning/src/graph_search/astar_planning_test_node.cpp
+++ robot_planning/src/graph_search/astar_planning_test_node.cpp
@@ -4,12 +4,13 @@
 #include <vector>
 #include "map/map_builder.h"
 #include "roadmap/roadmap_data_structures.h"
-#include "roadmap/roadmap_visualization.h" // <--- 1. INCLUDE NECESSARIO
+#include "roadmap/roadmap_visualization.h"
 #include "combinatorial_planning/exact_cell_decomposition.h"
 #include "combinatorial_planning/approximate_cell_decomposition.h"
 #include "combinatorial_planning/maximum_clearance_roadmap.h"
 #include "sample_based_planning/prm.h"
 #include "sample_based_planning/rrt.h"
+#include "sample_based_planning/rrt_star.h" // Assicurati che sia incluso
 #include "graph_search/astar.h" 
 
 using namespace std;
@@ -19,14 +20,26 @@ shared_ptr<Roadmap> generateRoadmap(const string& type, const Map& map) {
         sample_planning::PRMConfig config;
         config.num_samples = 1500; config.k_neighbors = 15;
         return sample_planning::buildPRM(map, config);
-    } else if (type == "ecd") return ExactDecomposition::exactCellDecomposition(map);
+    } 
+    else if (type == "ecd") return ExactDecomposition::exactCellDecomposition(map);
     else if (type == "acd") return ApproximateDecomposition::approximateCellDecomposition(map, 4);
     else if (type == "mcr") return MaxClearanceRoadmap::maximumClearanceRoadmap(map);
     else if (type == "rrt") {
-        sample_planning::RRTConfig config; config.max_iterations = 2000; config.step_size = 1.0;
+        sample_planning::RRTConfig config; config.max_iterations = 2500; config.step_size = 0.5;
         return sample_planning::buildRRT(map, config);
     }
-    return nullptr;
+    else if (type == "rrt_star") {
+        sample_planning::RRTStarConfig config; 
+        config.max_iterations = 2500; 
+        config.step_size = 0.5;
+        config.search_radius = 2.0;
+        return sample_planning::buildRRTStar(map, config);
+    }
+    // -----------------------------------------------
+    else {
+        ROS_ERROR("Planner Type '%s' NOT RECOGNIZED!", type.c_str());
+        return nullptr;
+    }
 }
 
 int main(int argc, char **argv) {
@@ -41,7 +54,10 @@ int main(int argc, char **argv) {
         Map map = builder.buildMap();
 
         shared_ptr<Roadmap> roadmap = generateRoadmap(planner_type, map);
-        if (!roadmap || roadmap->getNumVertices() == 0) return 1;
+        if (!roadmap || roadmap->getNumVertices() == 0) {
+            ROS_ERROR("Failed to generate roadmap for type: %s", planner_type.c_str());
+            return 1;
+        }
 
         // Conversione esplicita Point -> Vertex
         Point s = map.start.get_position();
@@ -57,7 +73,6 @@ int main(int argc, char **argv) {
         
         ROS_INFO("Mission Sequence Calculated. Executing A*...");
         
-        // --- 2. ACCUMULO DEL PERCORSO ---
         vector<int> fullGlobalPath; 
         bool missionSuccess = true;
 
@@ -72,26 +87,19 @@ int main(int argc, char **argv) {
                 break;
             } else {
                 ROS_INFO("Segment %lu OK: %d -> %d (%lu steps)", i, from, to, segment.size());
-                // Uniamo il segmento al percorso globale
                 fullGlobalPath.insert(fullGlobalPath.end(), segment.begin(), segment.end());
             }
         }
 
-        // --- 3. VISUALIZZAZIONE FINALE ---
         if (missionSuccess) {
             ROS_INFO("MISSION COMPLETE! Total path nodes: %lu", fullGlobalPath.size());
             ROS_INFO("Generating visualization...");
             
             roadmap_viz::RoadmapVisualizer viz;
-            
-            // Renderizza base (Map + Roadmap + Vittime)
             viz.render(map, *roadmap);
-            
-            // Sovrapponi il percorso completo (Rosso)
             viz.drawPath(*roadmap, fullGlobalPath);
             
-            // Salva l'immagine
-            // NOTA: Assicurati che la cartella 'test_results' esista dentro src/robot_planning/
+            // Assicurati che questa cartella esista
             string filename = "src/robot_planning/robot_planning/src/graph_search/test/mission_" + planner_type + ".png";
             if (viz.saveToFile(filename)) {
                 ROS_INFO("Visualization saved to: %s", filename.c_str());
